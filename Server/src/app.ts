815c112672669c292b4ae230c95382829575332e
Laveen kumar <104753703+Lavin-kulal@users.noreply.github.com>
2025-07-19 12:29:58 +0530

    Update app.ts

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -10,8 +10,19 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { rateLimit as expressRateLimit } from "express-rate-limit";
 import { RegisterRoutes } from "./routes/routes";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+
 const app = express();
 const corsOptions = {
   origin: "http://localhost:3000",
